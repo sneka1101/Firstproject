2e5b0cca3dde53920db3bdfe0a02f2bf9c96e271
sneka1101 <109452061+sneka1101@users.noreply.github.com>
2023-08-20 13:53:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 
 import './App.css';
-import Home from './Home';
-
-
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import Menu from './Pages/Menu';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
 
 function App() {
   return (
     <div className="App">
-      <Home/>
-     
+      <Router>
+        <Navbar />
+        <Routes>
+         
+          <Route path='/'exact Component={Home}/>
+          <Route path='/menu'exact Component={Menu}/>
+          <Route path='/about'exact Component={About}/>
+          <Route path='/contact'exact Component={Contact}/>
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
 }
-  
+
 export default App;
